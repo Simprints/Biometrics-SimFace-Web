2f8ddb8582b5c8879e3b691f7063ccdd9ac8c49e
Sam Merrick <sam.merrick@simprints.com>
2025-09-16 16:26:14 +0100

    fix images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import ClientPageWrapper from './dynamicclientpage';
 
 const getImages = (dir: string): string[] => {
   const directory = path.join(process.cwd(), 'public', 'images', dir);
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
   if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory, { recursive: true });
     return [];
   }
   const filenames = fs.readdirSync(directory);
-  return filenames.map((name) => `/images/${dir}/${name}`);
+  return filenames.map((name) => `${basePath}/images/${dir}/${name}`);
 };
 
 export default function Page() {
